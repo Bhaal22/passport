63068f570a31e54d327f0487a7f1cd8ff1d22f2a
Bhaal22 <muller.john@gmail.com>
2017-11-06 14:09:59 +0100

    first remaining dates computation

--- tictac/tictac.py
+++ tictac/tictac.py
@@ -1,15 +1,38 @@
 import argparse
+import os
+import json
+from datetime import date, datetime
+from pprint import pprint
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Compute the remaining times for your objectives')
-    parser.add_argument('objectives', type=str,
-                   help='an integer for the accumulator')
+    parser.add_argument('--profile', type=str,
+                   help='profile path')
 
     args = parser.parse_args()
+    return args
+
+
+def handle_objectives(data):
+    objectives_data = data['objectives']
+
+    today = datetime.now()
+    for objective in objectives_data:
+        objective_date = datetime.strptime(objective['date'], '%Y-%m-%d')
+        dt = objective_date - today
+        pprint(dt)
+
 
 def main():
-    parse_arguments()
+    args = parse_arguments()
+
+    profile = args.profile
+    objectives = os.path.join(profile, 'objectives.json')
+
+    with open(objectives) as data_file:
+        data = json.load(data_file)
+        handle_objectives(data)
 
 
 if __name__ == "__main__":
